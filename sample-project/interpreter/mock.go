284ae4341569fbc93f8a71c178d512ea994852f7
yukatayu <yukatayu.dev@gmail.com>
2020-09-13 22:15:27 +0900

    add: sample/interpreter

---
+++ sample-project/interpreter/mock.go
@@ -0,0 +1,29 @@
+package interpreter
+
+import (
+	. "github.com/go-utils/go-repo-gen"
+)
+
+func MockDB() Interpreter {
+	return Interpreter{
+		Insert: func(effect Insert) RuntimeContext {
+			// tableName, newValue
+			_, newValue := effect.InsertionParam()
+			return RuntimeContext{
+				OK:  true,
+				Err: nil,
+				Ctx: newValue,
+			}
+		},
+
+		Select: func(effect Select) RuntimeContext {
+			// tableName, existingValue, limit
+			_, existingValue, _ := effect.SelectionParam()
+			return RuntimeContext{
+				OK:  true,
+				Err: nil,
+				Ctx: existingValue,
+			}
+		},
+	}
+}
---
+++ sample-project/interpreter/pseudoDB.go
@@ -0,0 +1,36 @@
+package interpreter
+
+import (
+	"fmt"
+	. "github.com/go-utils/go-repo-gen"
+)
+
+func PseudoDB() Interpreter {
+	return Interpreter{
+		Insert: func(effect Insert) RuntimeContext {
+			tableName, newValue := effect.InsertionParam()
+			fmt.Printf("[D] PseudoDB | Insert | tableName: %s, inserting: %v\n", tableName, newValue)
+			return RuntimeContext{OK: true}
+		},
+		Select: func(effect Select) RuntimeContext {
+			tableName, existingValue, limit := effect.SelectionParam()
+			fmt.Printf("[D] PseudoDB | Update | tableName: %s, getting: %s, limit: %d\n", tableName, existingValue, limit)
+			return RuntimeContext{OK: true}
+		},
+	}
+}
+
+// 以下は必須ではないが，DB固有機能を使うような便利ツールの類
+
+func PseudoDB_Transaction(effects []DBEffect) ([]Any, error){
+	DB := PseudoDB()
+	var resList []Any
+	// TODO: Begin Transaction Here !!
+	resList, err := GetNotNilList(effects, DB)
+	if(err != nil){
+		// Rollback Here !!
+		return resList, err
+	}
+	// TODO: Commit Transaction Here !!
+	return resList, nil
+}
